5fa4922955a12aa76fd490a0e1f68cd39c354a6f
Kanto2113 <kanto2113@gmail.com>
2020-04-11 18:16:38 -0700

    hardcoded player characters

--- src/App.js
+++ src/App.js
@@ -9,13 +9,25 @@ const defaultCharacterArray = [{
   characterName: "Dyce",
   initiative: 0,
   activeTurn: true,
-  avatarURL: "https://i.pinimg.com/originals/7e/85/17/7e8517acd27a3d13a18704708892afc9.png"
+  avatarURL: "https://lh3.googleusercontent.com/proxy/Li4vRZM13iqjgQ5D5D4yvWTgfXwOHa03VsXCI2lq6yE-793IwmgwQk6m3nl5z6a86X3C1xX5RH6leh0OWTMFG6QkiZ8KqwLmYK7lEmOKmOiHvNdg-GVSF2-5PtBh"
 },
 {
   characterName: "Elad",
   initiative: 0,
   activeTurn: false,
   avatarURL: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/6/369/420/618/636272705936709430.png"
+},
+{
+  characterName: "Vashti",
+  initiative: 0,
+  activeTurn: false,
+  avatarURL: "https://i.redd.it/7j8sck3bw6d21.jpg"
+},
+{
+  characterName: "Durin",
+  initiative: 0,
+  activeTurn: false,
+  avatarURL: "https://p1.hiclipart.com/preview/349/53/139/elf-dungeons-dragons-high-elf-ranger-d20-system-fantasy-warrior-roleplaying-game-png-clipart.jpg"
 }
 ]
 
